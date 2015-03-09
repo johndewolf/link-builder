8f831de2904d076103e67a4f92515bed2472dc7d
John DeWolf <jdewolf@backupify.com>
2015-03-09 18:11:44 -0400

    initial setup

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("#twitterSubmit").on("click", function() {
+		$( "#twitterComplete" ).text( "http://twitter.com/intent/tweet?url=http://" + $("#twitterUrl").val() + "&amp;text=" + $("#twitterCopy").val().replace(/ /g, "%20")); 
+	});
+});
