7a479951b262c4ee692881fc4ddc3f7d0f8a3c67
johndewolf <jdewolf06@gmail.com>
2015-03-09 21:09:43 -0400

    basic setup for facebook

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 	$("#twitterSubmit").on("click", function() {
-		$( "#twitterComplete" ).text( "http://twitter.com/intent/tweet?url=http://" + 
-				$("#twitterUrl").val() + "&amp;text=" + 
-				$("#twitterCopy").val().replace(/ /g, "%20")); 
+		$( "#twitterComplete" ).text( "http://twitter.com/intent/tweet?url=http://" +
+				$("#twitterUrl").val() + "&amp;text=" +
+				$("#twitterCopy").val().replace(/ /g, "%20"));
 	});
+
+  $("#facebookSubmit").on("click", function() {
+    $("#facebookComplete").text("https://www.facebook.com/sharer/sharer.php?u="+$("#facebookURL").val());
+  });
 });
