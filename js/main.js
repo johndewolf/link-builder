4f2e42592c2c056dec4214fe75253ddaf153502a
John DeWolf <jdewolf@backupify.com>
2015-03-24 11:41:23 -0400

    added gplus and linkedin

--- js/main.js
+++ js/main.js
@@ -8,18 +8,20 @@ linkBuilder.controller('mainController', ['$scope', '$log', function($scope, $lo
 		campaign: '',
 		medium: '',
 		twitterSource: '',
-		facebookSource: ''
+		facebookSource: '',
+		linkedinSource: '',
+		gplusSource: ''
 	}
 
 	
 	$scope.twitterCopy = '';
 
-	$scope.twitterComplete = function() {
-		return $scope.twitterCopy.replace(/ /g, "%20");
+	function replaceSpaces(copy) {
+		return copy.replace(/ /g, "%20");
 	};
 
 	$scope.twitterReturnUrl = function() {
-		return 'http://twitter.com/intent/tweet?text='+ $scope.twitterComplete() + '&' + 'url=' + $scope.url + '?utm_source=' + $scope.utm.twitterSource + '&utm_campaign=' + $scope.utm.campaign + '&utm_medium=' + $scope.utm.medium ;
+		return 'http://twitter.com/intent/tweet?text='+ replaceSpaces($scope.twitterCopy) + '&' + 'url=' + $scope.url + '?utm_source=' + $scope.utm.twitterSource + '&utm_campaign=' + $scope.utm.campaign + '&utm_medium=' + $scope.utm.medium ;
 	}
 
 	$scope.tweetFullChara = function() {
@@ -30,6 +32,17 @@ linkBuilder.controller('mainController', ['$scope', '$log', function($scope, $lo
 		return 'http://www.facebook.com/sharer/sharer.php?u=' + $scope.url + '?utm_source=' + $scope.utm.facebookSource + '&utm_campaign=' + $scope.utm.campaign + '&utm_medium=' + $scope.utm.medium ;
 	}
 
-	$scope.gplus = '';
+	$scope.linkedin = {
+		title: '',
+		summary: ''
+	}
+
+	$scope.linkedinReturnUrl = function() {
+		return 'http://www.linkedin.com/shareArticle?mini=true&url=' + $scope.url + '&title=' + replaceSpaces($scope.linkedin.title) + '&summary=' + replaceSpaces($scope.linkedin.summary) + '?utm_source=' + $scope.utm.linkedinSource + '&utm_campaign=' + $scope.utm.campaign + '&utm_medium=' + $scope.utm.medium ; 
+	}
+
+	$scope.gplusReturnUrl = function(){
+		return 'https://plus.google.com/share?url=' + $scope.url + '?utm_source=' + $scope.utm.gplusSource + '&utm_campaign=' + $scope.utm.campaign + '&utm_medium=' + $scope.utm.medium;
+	}
 
 }]); 
