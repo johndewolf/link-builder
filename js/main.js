1892860cf072a54990ddb3ec735bc2297beb4f99
John DeWolf <jdewolf@backupify.com>
2015-03-24 10:46:27 -0400

    update to controller and style

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ linkBuilder.controller('mainController', ['$scope', '$log', function($scope, $lo
 
 	$scope.url = '';
 
-	$scope.twitterUrl = function() {
-		if ($scope.twitterUrlInput != '') {
-			return 'url=http://' + $scope.url;
-		};
+	$scope.utm = {
+		campaign: '',
+		medium: '',
+		twitterSource: '',
+		facebookSource: ''
 	}
+
 	
 	$scope.twitterCopy = '';
 
@@ -16,8 +18,16 @@ linkBuilder.controller('mainController', ['$scope', '$log', function($scope, $lo
 		return $scope.twitterCopy.replace(/ /g, "%20");
 	};
 
+	$scope.twitterReturnUrl = function() {
+		return 'http://twitter.com/intent/tweet?text='+ $scope.twitterComplete() + '&' + 'url=' + $scope.url + '?utm_source=' + $scope.utm.twitterSource + '&utm_campaign=' + $scope.utm.campaign + '&utm_medium=' + $scope.utm.medium ;
+	}
+
 	$scope.tweetFullChara = function() {
 		return $scope.twitterCopy.length + $scope.url.length + 9;
+	}
+
+	$scope.facebookReturnUrl = function() {
+		return 'http://www.facebook.com/sharer/sharer.php?u=' + $scope.url + '?utm_source=' + $scope.utm.facebookSource + '&utm_campaign=' + $scope.utm.campaign + '&utm_medium=' + $scope.utm.medium ;
 	}
 
 	$scope.gplus = '';
