4c05b7681f847044ba9436f1ad800c765db39951
John DeWolf <jdewolf@backupify.com>
2015-03-30 17:50:29 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+var linkBuilder = angular.module('linkBuilder', []);
+
+linkBuilder.controller('mainController', ['$scope', '$log', function($scope, $log) {
+
+	$scope.url = '';
+
+	$scope.utm = {
+		campaign: '',
+		medium: '',
+		twitterSource: '',
+		facebookSource: '',
+		linkedinSource: '',
+		gplusSource: ''
+	}
+
+	
+	$scope.twitterCopy = '';
+
+	function replaceSpaces(copy) {
+		return copy.replace(/ /g, "%20");
+	};
+
+	function utmBuild(socialNetwork) {
+		var medium = $scope.medium;
+		var campaign = $scope.campaign;
+		var scope = $scope.utm + '.' + socialNetwork + 'Source';
+	}
+
+
+	$scope.twitterReturnUrl = function() {
+		return 'http://twitter.com/intent/tweet?text='+ replaceSpaces($scope.twitterCopy) + '&' + 'url=' + $scope.url;
+	}
+
+	$scope.tweetFullChara = function() {
+		return $scope.twitterCopy.length + $scope.url.length + 9;
+	}
+
+	$scope.facebookReturnUrl = function() {
+		return 'http://www.facebook.com/sharer/sharer.php?u=' + $scope.url;
+	}
+
+	$scope.utmBuilder = utmBuild('gplus');
+
+
+	$scope.linkedin = {
+		title: '',
+		summary: ''
+	}
+
+	$scope.linkedinReturnUrl = function() {
+		return 'http://www.linkedin.com/shareArticle?mini=true&url=' + $scope.url + '&title=' + replaceSpaces($scope.linkedin.title) + '&summary=' + replaceSpaces($scope.linkedin.summary); 
+	}
+
+	$scope.gplusReturnUrl = function(){
+		return 'https://plus.google.com/share?url=' + $scope.url;
+	}
+
+}]); 
+
+
+// myApp.directive("twitterUrl", function() {
+//     return {
+//         template: ''
+//     }
+// });
