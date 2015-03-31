57f301aa76a3e13ea9085595c6dbc05f220c662b
John DeWolf <jdewolf@backupify.com>
2015-03-31 08:25:36 -0400

    fix merge conflict

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 var linkBuilder = angular.module('linkBuilder', []);
 
 linkBuilder.controller('mainController', ['$scope', '$log', function($scope, $log) {
-<<<<<<< HEAD
 
-=======
 	if($scope.url === undefined){
   	return false;
   }
-  
->>>>>>> gh-pages
+ 
 	$scope.url = '';
 
 	$scope.utm = {
